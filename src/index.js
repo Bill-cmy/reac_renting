cff92419c6e5d4e5a27049715fd6a9c7f036966a
cmy <2411493277>
2020-11-03 00:19:39 +0800

    项目初始化完成，引入ui组件和font图标完成

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './assets/fonts/iconfont.css'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
